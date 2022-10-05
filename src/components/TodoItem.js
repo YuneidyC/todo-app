cabd4c34ea981302711fb8ac85f612d10eb8223e
YuneidyC <yuneidycastillo25@gmail.com>
2022-10-05 22:11:58 +0100

    Refactor TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,14 +3,13 @@ import '../styles/TodoItem.css';
 
 
 function TodoItem(props) {
-
     return (
-        <li className="TodoItem">
+        <li className={`TodoItem ${props.completed && 'TodoItem--completed'}`}>
             <label className="checklist">
-                <input type="checkbox" name="radio" className={`input--check ${props.completed && 'Icon-check--active'}`} />
-                <span className="checkmark"></span>
+                <input type="checkbox" name="radio" className={`input--check ${props.completed && 'input-check--active'}`} />
+                <span className={`checkmark  ${props.completed && 'span-check--active'}`}></span>
             </label>
-            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
+            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
             <img className="Icon--image" src="https://img.icons8.com/cute-clipart/64/000000/delete.png" alt="trash" />
         </li>
     )
