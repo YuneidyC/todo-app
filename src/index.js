5daf8bc8912cb1bf7141a073230c30caca7c17fc
YuneidyC <yuneidycastillo25@gmail.com>
2022-10-07 13:51:02 +0100

    Fix react-dom import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOMClient from 'react-dom/client';
 // import './index.css';
 import App from './App/index';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(<App />);
-
