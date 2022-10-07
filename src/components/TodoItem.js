ad26d55dec4501d4b5d6f40e635621664c56f675
YuneidyC <yuneidycastillo25@gmail.com>
2022-10-07 18:52:03 +0100

    Add TodoItem button's functionality

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,10 +7,10 @@ function TodoItem(props) {
         <li className={`TodoItem ${props.completed && 'TodoItem--completed'}`}>
             <label className="checklist">
                 <input type="checkbox" name="radio" className={`input--check ${props.completed && 'input-check--active'}`} />
-                <span className={`checkmark  ${props.completed && 'span-check--active'}`}></span>
+                <span className={`checkmark  ${props.completed && 'span-check--active'}`} onClick={props.onComplete}></span>
             </label>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
-            <img className="Icon--image" src="https://img.icons8.com/cute-clipart/64/000000/delete.png" alt="trash" />
+            <img className="Icon--image" src="https://img.icons8.com/cute-clipart/64/000000/delete.png" alt="trash" onClick={props.onDelete} />
         </li>
     )
 }
