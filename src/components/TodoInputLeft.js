b0ed9140ae18fe17693b9e864d6728998ebf435c
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2022-10-14 14:54:28 +0100

    Add TodoInputLeft component and style

---
+++ src/components/TodoInputLeft.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/TodoInputLeft.css';
+
+function TodoInputLeft() {
+    return (
+        <input className="TodoInputLeft" type="text" placeholder="Add your task" />
+    )
+}
+
+export { TodoInputLeft };
