831a532926dbed533c6f23e422135365d1f85140
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2022-10-14 16:39:31 +0100

    Add TodoFieldLeft component and style
    It replaces TodoInputLeft and TodoButtonLeft

---
+++ src/components/TodoFieldLeft.js
@@ -0,0 +1,31 @@
+import React from "react";
+import '../styles/TodoFieldLeft.css';
+
+import { TodoContext } from "../TodoContext";
+
+function TodoFieldLeft() {
+    const [newTodoValue, setNewTodoValue] = React.useState('');
+
+    const {
+        addTodo
+    } = React.useContext(TodoContext);
+
+    const onChange = (event) => {
+        setNewTodoValue(event.target.value);
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addTodo(newTodoValue);
+        setNewTodoValue('');
+    };
+
+    return (
+        <div className="TodoFieldLeft">
+            <input className="TodoFieldLeft--input" onChange={onChange} type="text" placeholder="Add a task" />
+            <button className="TodoFieldLeft--button" onClick={onSubmit}>Create a task</button>
+        </div>
+    )
+}
+
+export { TodoFieldLeft };
