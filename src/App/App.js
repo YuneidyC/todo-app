12157a434a4c58235d84c987b4709069ce924b1c
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2022-10-14 14:51:16 +0100

    Add media query

--- src/App/App.js
+++ src/App/App.js
@@ -1,64 +1,17 @@
 import './App.css';
 import React from 'react';
-import { TodoCounter } from '../components/TodoCounter';
-import { TodoSearch } from '../components/TodoSearch';
-import { TodoHeader } from '../components/TodoHeader';
-import { TodoList } from '../components/TodoList';
-import { TodoItem } from '../components/TodoItem';
-import { TodoForm } from '../components/TodoForm';
-import { CreateTodoButton } from '../components/CreateTodoButton';
-import { TodoContext } from '../TodoContext';
-import { Modal } from '../components/Modal';
+import { useMediaQuery } from 'usehooks-ts';
+import { TodoProvider } from '../TodoContext';
+import { Mobile } from './Mobile';
+import { Desktop } from './Desktop';
 
 function App() {
-    const {
-        error,
-        loading,
-        searchedTodos,
-        completeTodo,
-        deleteTodo,
-        openModal,
-        setOpenModal
-    } = React.useContext(TodoContext);
+    const matches = useMediaQuery('(min-width: 1000px)');
 
     return (
-        <React.Fragment>
-            <TodoHeader />
-            <TodoCounter />
-            <TodoSearch />
-            <TodoList>
-                <div className='TodoList--container'>
-                    {error && <p>Error</p>}
-                    {loading &&
-                        <div className='ring-loading'>
-                            Loading...
-                            <span className='ring-loading__span'></span>
-                        </div>}
-                    {(!loading && !searchedTodos.length) && <p>Create your first TODO!</p>}
-
-                    {searchedTodos.map((todo, index) => (
-                        <TodoItem
-                            key={index}
-                            text={todo.text}
-                            completed={todo.completed}
-                            onComplete={() => completeTodo(todo.text)}
-                            onDelete={() => deleteTodo(todo.text)}
-                        />
-                    ))}
-                </div>
-            </TodoList>
-
-            {openModal && (
-                <Modal>
-                    <TodoForm />
-                </Modal>
-            )}
-
-            <CreateTodoButton
-                openModal={openModal}
-                setOpenModal={setOpenModal}
-            />
-        </React.Fragment>
+        <TodoProvider>
+            {matches ? <Desktop /> : <Mobile />}
+        </TodoProvider>
     );
 }
 
