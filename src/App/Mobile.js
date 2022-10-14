b43d184c2b289f3b6d9d8ca5ff8ca31968dfbc66
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2022-10-14 14:52:14 +0100

    Add Mobile media query

---
+++ src/App/Mobile.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { TodoCounter } from '../components/TodoCounter';
+import { TodoSearch } from '../components/TodoSearch';
+import { TodoHeader } from '../components/TodoHeader';
+import { TodoList } from '../components/TodoList';
+import { TodoItem } from '../components/TodoItem';
+import { TodoForm } from '../components/TodoForm';
+import { CreateTodoButton } from '../components/CreateTodoButton';
+import { TodoContext } from '../TodoContext';
+import { Modal } from '../components/Modal';
+
+function Mobile() {
+    const {
+        error,
+        loading,
+        searchedTodos,
+        completeTodo,
+        deleteTodo,
+        openModal,
+        setOpenModal
+    } = React.useContext(TodoContext);
+
+    return (
+        <React.Fragment>
+            <TodoHeader />
+            <TodoCounter />
+            <TodoSearch />
+            <TodoList>
+                <div className='TodoList--container'>
+                    {error && <p>Error</p>}
+                    {loading &&
+                        <div className='ring-loading'>
+                            Loading...
+                            <span className='ring-loading__span'></span>
+                        </div>}
+                    {(!loading && !searchedTodos.length) && <p>Create your first TODO!</p>}
+
+                    {searchedTodos.map((todo, index) => (
+                        <TodoItem
+                            key={index}
+                            text={todo.text}
+                            completed={todo.completed}
+                            onComplete={() => completeTodo(todo.text)}
+                            onDelete={() => deleteTodo(todo.text)}
+                        />
+                    ))}
+                </div>
+            </TodoList>
+
+            {openModal && (
+                <Modal>
+                    <TodoForm />
+                </Modal>
+            )}
+
+            <CreateTodoButton
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+            />
+        </React.Fragment>
+        );
+}
+
+export { Mobile };
