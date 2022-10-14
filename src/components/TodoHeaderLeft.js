4ff2ce0c74c7e41c195ce968a4189a8777547770
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2022-10-14 14:52:55 +0100

    Add TodoHeaderLeft component and style

---
+++ src/components/TodoHeaderLeft.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../styles/TodoHeaderLeft.css';
+
+function TodoHeaderLeft() {
+    return (
+        <h2 className="TodoHeaderLeft">Create a new task!</h2>
+    )
+}
+
+export { TodoHeaderLeft };
+
+
+
+
+
+
+
+
+
+
+
+
+
