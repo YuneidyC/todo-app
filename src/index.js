8b21cee51f480f6c9575a7f80eaece464ece9684
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2022-10-14 14:05:37 +0100

    Change App render file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 // import './index.css';
-import App from './App/index';
+import { App } from './App/App';
 
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(<App />);
