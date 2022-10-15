893ceb7c1dfc089bf1b9a25075ba4bd9ec720b0e
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2022-10-15 18:38:02 +0100

    Fix typo

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -4,7 +4,7 @@ import '../styles/TodoHeader.css';
 function TodoHeader() {
     return (
         <div className="TodoHeader">
-            <p className="TodoHeader--p">Your task</p>
+            <p className="TodoHeader--p">Your tasks</p>
         </div>
     );
 }
